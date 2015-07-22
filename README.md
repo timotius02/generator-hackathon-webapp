268416d3a16304f6383efe2cfcfac79920747740
Timotius Sitorus <timsitorus1@gmail.com>
2015-07-22 13:55:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# generator-hackathon-webapp 
+
+> [Yeoman](http://yeoman.io) generator for quickly building modern web apps using Gulp, Browserify, and Babel
+
+## What's inside?
+
+Bundled:
+
+* [Gulp](http://gulpjs.com/)
+* [Bower](http://bower.io/)
+* [Browserify](http://browserify.org/) with Watchify support 
+* livereload via [BrowserSync](http://www.browsersync.io/)
+* [Mocha](http://mochajs.org/) for testing
+* Sourcemaps for styles and scripts
+
+Optional: 
+
+* Sass Compilation via [Libsass](http://libsass.org/)
+* [Twitter Bootstrap](http://getbootstrap.com/) - Sass/Css version
+* [Modernizr](http://modernizr.com/)
+* ES6 & React Support via [Babel](https://babeljs.io/)
+* [jQuery](https://jquery.com/)
+* Quick deploy to [Github Pages](https://pages.github.com/)
+
+## Requirements
+
+* node.js >= 0.12
+* Node-Sass >= 3.2 for Sass compilation
+
+If you need to install node-sass
+```
+$ npm install node-sass
+```
+
+
+## Getting Started
+
+```
+$ npm install -g yo                                # Install Yeoman (if you don't have it yet)...
+$ npm install -g generator-hackathon-webapp   	   # ...then install this generator...
+$ yo generator-hackathon-webapp                    # ...and run it.
+```
+
+## Gulp Tasks
+
+- Run `gulp serve` to preview and watch for changes
+- Run `gulp serve:test` to run the tests in the browser
+- Run `gulp` to build your webapp for production
+- Run `gulp serve:dist` to preview the production build
+
+## License
+
+MIT
---
+++ app/templates/gulpfile.babel.js
@@ -0,0 +1,250 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import del from 'del';
+import { stream as wiredep } from 'wiredep';
+import through2 from 'through2';
+import browserify from 'browserify';
+import watchify from 'watchify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+// Source and target JS files for Browserify
+const appDir = './app/';
+const jsMainFile = appDir + 'scripts/main.js';
+const jsBundleFile = 'main.js';
+
+gulp.task('styles', () => {<% if (includeSass) { %>
+  return gulp.src('app/styles/*.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError))<% } else { %>
+  return gulp.src('app/styles/*.css')
+    .pipe($.sourcemaps.init())<% } %>
+    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
+});
+
+const bundler = browserify({
+  entries: [jsMainFile], <% if (includeES6) { %>
+  transform: [babelify],<% } %>
+  debug: true,
+  cache: {},
+  packageCache: {},
+  fullPaths: true // for watchify
+});
+
+// Build JavaScript using Browserify
+gulp.task('scripts', () => {
+  return bundler
+    .bundle()
+    .pipe(source(jsBundleFile))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('watchify', () => {
+  const watcher = watchify(bundler);
+  return watcher
+    .on('error', function (err) {
+      $.util.log(err.toString());
+      this.emit('end');
+    })
+    .on('update', () => {
+      watcher.bundle()
+        .pipe(source(jsBundleFile))
+        .pipe(buffer())
+        .pipe($.sourcemaps.init({
+          loadMaps: true
+        })) // loads map from browserify file
+        .pipe($.sourcemaps.write('./')) // writes .map file
+        .pipe(gulp.dest('.tmp/scripts'))
+        .pipe(reload({
+          stream: true
+        }));
+
+      $.util.log("Updated JavaScript sources");
+
+    })
+    .bundle() // Create the initial bundle when starting the task
+    .pipe(source(jsBundleFile))
+    .pipe(gulp.dest('.tmp/scripts'));
+});
+
+function lint(files, options) {
+  return () => {
+    return gulp.src(files)
+      .pipe(reload({
+        stream: true,
+        once: true
+      }))
+      .pipe($.eslint(options))
+      .pipe($.eslint.format())
+      .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+  };
+}
+
+const testLintOptions = {
+  env: {
+<% if (testFramework === 'mocha') { -%>
+    mocha: true
+<% } else if (testFramework === 'jasmine') { -%>
+    jasmine: true
+<% } -%>
+  }
+};
+
+gulp.task('lint', lint('app/scripts/**/*.js'));
+gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
+
+gulp.task('html', ['styles'], () => {
+  const assets = $.useref.assets({
+    searchPath: ['.tmp', 'app', '.']
+  });
+
+  return gulp.src('app/*.html')
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.minifyCss({
+      compatibility: '*'
+    })))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.if('*.html', $.minifyHtml({
+      conditionals: true,
+      loose: true
+    })))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('app/images/**/*')
+    .pipe($.if($.if.isFile, $.cache($.imagemin({
+        progressive: true,
+        interlaced: true,
+        // don't remove IDs from SVGs, they are often used
+        // as hooks for embedding and styling
+        svgoPlugins: [{
+          cleanupIDs: false
+        }]
+      }))
+      .on('error', function(err) {
+        console.log(err);
+        this.end();
+      })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src(require('main-bower-files')({
+      filter: '**/*.{eot,svg,ttf,woff,woff2}'
+    }).concat('app/fonts/**/*'))
+    .pipe(gulp.dest('.tmp/fonts'))
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', () => {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('serve', ['styles','watchify', 'fonts'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch([
+    'app/*.html',
+    'app/images/**/*',
+    '.tmp/fonts/**/*'
+  ]).on('change', reload);
+
+  gulp.watch(['app/styles/**/*.scss', '/bower_components'], ['styles']);
+  gulp.watch('app/fonts/**/*', ['fonts']);
+  gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
+gulp.task('serve:dist', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['dist']
+    }
+  });
+});
+
+gulp.task('serve:test', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    ui: false,
+    server: {
+      baseDir: 'test',
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch('test/spec/**/*.js').on('change', reload);
+  gulp.watch('test/spec/**/*.js', ['lint:test']);
+});
+
+// inject bower components
+gulp.task('wiredep', () => {<% if (includeSass) { %>
+  gulp.src('app/styles/*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/styles'));
+<% } %>
+  gulp.src('app/*.html')
+    .pipe(wiredep({<% if (includeBootstrap) { if (includeSass) { %>
+      exclude: ['bootstrap-sass'],<% } else { %>
+      exclude: ['bootstrap.js'],<% }} %>
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
+});
+<% if (includeGHPages) {%>
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe($.ghPages()); // https://www.npmjs.com/package/gulp-gh-pages
+});
+<% } %>
+
+gulp.task('build', ['scripts', 'lint', 'html', 'images', 'fonts', 'extras'], () => {
+  return gulp.src('dist/**/*').pipe($.size({
+    title: 'build',
+    gzip: true
+  }));
+});
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
+});
